8c5b93ec03d424ca723844093b3aa65b011ae723
Nishanth R <nishanthr720@gmail.com>
2020-09-22 22:19:50 +0530

    Added Login and Register Components and Routes

--- rest_api/angular-app/src/app/app.module.ts
+++ rest_api/angular-app/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+// forms
+import { FormsModule } from '@angular/forms';
+
+// routes
+import { AppRoutesModule } from './app-routes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
