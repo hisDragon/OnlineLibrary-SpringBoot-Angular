01b5e7e1c6f1f3207038c60cac6197b2fe117c55
Nishanth R <nishanthr720@gmail.com>
2020-10-03 21:37:16 +0530

    added recommended books

--- rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
+++ rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-recommended-books',
   templateUrl: './recommended-books.component.html',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecommendedBooksComponent implements OnInit {
 
+  
+
+  books_details : any = [
+    // { img_path : '../../../assets/textbooks/dbms/img.jpg',pdf_path:'../../assets/textbooks/dbms/text_book.pdf'},
+    // {
+    //   img_path : '../../../assets/textbooks/aptitude/img.jpg',pdf_path:'../../assets/textbooks/aptitude/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/oop/img.jpg',pdf_path:'../../assets/textbooks/oop/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/atc/img.jpg',pdf_path:'../../assets/textbooks/atc/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/ds/img.jpg',pdf_path:'../../assets/textbooks/ds/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/dc/img.jpg',pdf_path:'../../assets/textbooks/dc/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/os/img.jpg',pdf_path:'../../assets/textbooks/os/text_book.pdf'
+    // },
+    // {
+    //   img_path : '../../../assets/textbooks/dbms/img.jpg',pdf_path:'../../assets/textbooks/dbms/text_book.pdf'
+    // }
+  
+    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/competitive-programming.pdf' },
+    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/css3-cheatsheets.pdf' },
+    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/html5-cheatsheet.pdf' },
+    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/Java 2 Core Language Little Black Book.pdf' }
+
+  ];
+
   constructor() { }
 
+  
+
   ngOnInit(): void {
+  }
+
+  openNewTab(index): void {
+    window.open(this.books_details[index].pdf_path);
   }
 
 }
