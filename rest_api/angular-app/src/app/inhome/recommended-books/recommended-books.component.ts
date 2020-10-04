f10f740392f1e775fb8323cfbb2f778b68a04638
Nishanth R <nishanthr720@gmail.com>
2020-10-04 13:24:00 +0530

    changed navbar angular-material

--- rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
+++ rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
@@ -11,33 +11,33 @@ export class RecommendedBooksComponent implements OnInit {
   
 
   books_details : any = [
-    // { img_path : '../../../assets/textbooks/dbms/img.jpg',pdf_path:'../../assets/textbooks/dbms/text_book.pdf'},
-    // {
-    //   img_path : '../../../assets/textbooks/aptitude/img.jpg',pdf_path:'../../assets/textbooks/aptitude/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/oop/img.jpg',pdf_path:'../../assets/textbooks/oop/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/atc/img.jpg',pdf_path:'../../assets/textbooks/atc/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/ds/img.jpg',pdf_path:'../../assets/textbooks/ds/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/dc/img.jpg',pdf_path:'../../assets/textbooks/dc/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/os/img.jpg',pdf_path:'../../assets/textbooks/os/text_book.pdf'
-    // },
-    // {
-    //   img_path : '../../../assets/textbooks/dbms/img.jpg',pdf_path:'../../assets/textbooks/dbms/text_book.pdf'
-    // }
+    { img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'},
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    },
+    {
+      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+    }
   
-    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/competitive-programming.pdf' },
-    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/css3-cheatsheets.pdf' },
-    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/html5-cheatsheet.pdf' },
-    { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/Java 2 Core Language Little Black Book.pdf' }
+    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/competitive-programming.pdf' },
+    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/css3-cheatsheets.pdf' },
+    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/html5-cheatsheet.pdf' },
+    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/Java 2 Core Language Little Black Book.pdf' }
 
   ];
 
--- rest_api/angular-app/src/app/material/material.module.ts
+++ rest_api/angular-app/src/app/material/material.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 
 // importing materials
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 // import and export modules as array
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatListModule,
+  MatSidenavModule,
+  MatIconModule
 ];
 
 @NgModule({
