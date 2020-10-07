fb123e9719f3d38db382be62dcd456eee63a36ba
Nishanth R <nishanthr720@gmail.com>
2020-10-07 14:09:04 +0530

    added r-books component into navbar

--- rest_api/angular-app/src/app/app-routes.module.ts
+++ rest_api/angular-app/src/app/app-routes.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const APP_ROUTES: Routes = [
-    
+    { path: '', component: HomeComponent }  // home component
+
+    // { path: '**', component: PathNotFoundComponent }  // path not found
 ];
 
 @NgModule({
