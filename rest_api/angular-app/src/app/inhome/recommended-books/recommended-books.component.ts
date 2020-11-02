f894ac9a317f7b6175163854870e789174d11798
Nishanth R <nishanthr720@gmail.com>
2020-11-02 18:17:34 +0530

    styling changes for frontend for book display

--- rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
+++ rest_api/angular-app/src/app/inhome/recommended-books/recommended-books.component.ts
@@ -11,33 +11,28 @@ export class RecommendedBooksComponent implements OnInit {
   
 
   books_details : any = [
-    { img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'},
+    { img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'},
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     },
     {
-      img_path : '../../../assets/forest_fog_trees_126479_5100x6600.jpg',pdf_path:'../../assets/html5-cheatsheet.pdf'
+      img_path : '../../../assets/forest.jpg', pdf_path:'../../assets/html5-cheatsheet.pdf'
     }
-  
-    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/competitive-programming.pdf' },
-    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/css3-cheatsheets.pdf' },
-    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/html5-cheatsheet.pdf' },
-    // { img_path: '../../../assets/img/714tSjSc4RL.jpg', pdf_path: '../../../assets/pdf/Java 2 Core Language Little Black Book.pdf' }
 
   ];
 
